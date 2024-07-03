b445bad7c1e9d32629fe55cdadddc9c74d72b443
BatbayarEnkhbaatar <mr.btbman@gmail.com>
2024-07-03 11:41:13 -0400

    action server code is added

--- src/robotic_action/robotic_action/rest_api.py
+++ src/robotic_action/robotic_action/rest_api.py
@@ -6,18 +6,16 @@ import rclpy
 from rclpy.node import Node
 import threading
 import os
-threading.Thread(target=lambda: rclpy.init()).start()
+
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 
 # Implementation of Pytorch
-import robotic_action.robotic_transformer_pytorch.pytorch_launch as pytorchLaunch
+from robotic_transformer_pytorch import pytorch_launch as pytorchLaunch
+# import robotic_action.robotic_action.robotic_transformer_pytorch.pytorch_launch as pytorchLaunch
 
-import numpy as np
-
-
-
+threading.Thread(target=lambda: rclpy.init()).start()
 
 #################################################################################################3
 ###############################LAUNCHING THE MODEL#################################################3
@@ -32,12 +30,8 @@ import torch
 batch = 2
 frames = 1
 video = torch.randn(batch, 3, frames, 224, 224) # (batch, channels, frames, height, width)
-
-
-
 action_client = ActionClient(Node("rest_api_interface"), RoboticTasks, "task_server")
 app = Flask(__name__)
-
 @app.route("/welcome", methods=["GET"])
 def welcome():
     #  Create a goal message
@@ -45,31 +39,34 @@ def welcome():
     response_msg = "Welcome to the 4DiVision's Yaskawa Robot Arm Simulation!"
     # print(response_msg)
     return jsonify({'message': response_msg}), 200
-# UPLOAD_FOLDER
+
 
 @app.route("/task", methods=["POST"])
 def activate_task():
-    try:
+       
+    task_command = request.json.get('task_command') 
+    # Create a goal message with the received task number
+    goal = RoboticTasks.Goal()
+    result_action = pytorchLaunch.launchPytorch(
+        sampleVideo=video,
+        instructionText=task_command
+    )
+    print("HERE RT-1 RESULTS: ")
+    
+    area_result = []
+ 
+    for sublist in result_action:
+        area_result.append(sublist[0])
+        # print(command)
+        # goal.task_number = area_result
+    # Send the goal asynchronously to the task server using the action client
+    # action_client.send_goal_async(goal)
+    # response_msg = {'message': result_action}
+    return jsonify(str(area_result)), 200
+    # return "succeed"
+\
 
-        
-        task_command = request.json.get('task_command') 
-        # Create a goal message with the received task number
-        # goal = RoboticTasks.Goal()
-        result_action = pytorchLaunch.launchPytorch(
-            sampleVideo=video,
-            instructionText=task_command
-        )
-        print(task_command)
-        
-        # goal.task_sequences = task_command
-        # Send the goal asynchronously to the task server using the action client
-        # action_client.send_goal_async(goal)
-        response_msg = {'message': task_command}
-        return jsonify(result_action), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
-
-host_ip = "192.168.253.180"
+host_ip = "192.168.255.238"
 if __name__ == "__main__":
-    app.run(host=host_ip, debug=True)
+    app.run(host="localhost", debug=True)
--- src/robotic_action/robotic_action/robotic_transformer_pytorch/pytorch_launch.py
+++ src/robotic_action/robotic_action/robotic_transformer_pytorch/pytorch_launch.py
@@ -1,28 +1,28 @@
 # robotic_transformer_pytorch/pytorch_launch.py
 import torch
 import numpy as np
-from pathlib import Path
-from  robotic_transformer_pytorch import MaxViT, RT1
+# import   robotic_transformer_pytorch
+from robotic_transformer_pytorch.robotic_transformer_pytorch  import MaxViT, RT1
+
 import os
 import sys
 #################################################################################################3
-###############################LAUNCHING THE MODEL#################################################3
-###############################LAUNCHING THE MODEL#################################################3
-###############################LAUNCHING THE MODEL#################################################3
 ###############################LAUNCHING THE MODEL#################################################3
 #################################################################################################3
 
 def launchPytorch(sampleVideo, instructionText):
     ACTION_LOWER_BOUND = -1
     ACTION_UPPER_BOUND = 1
-    MODEL_PATH = os.path.join(sys.path[0])
+    # MODEL_NAME = 
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    print(script_dir)
+    # Specify the file name to access (change 'example.txt' to your file name)
+    model_name = "rt1_v1.pt"
 
-    print(MODEL_PATH)
-    # MODEL_PATH.mkdir(parents=True, exist_ok=True)
-    MODEL_NAME = "/tr1_model1.pt"
-    MODEL_SAVE_PATH = MODEL_PATH + MODEL_NAME
-    print("Currecnt directory is : ", MODEL_PATH)
-    print(f"TR-1 Model is loaded from {MODEL_SAVE_PATH}")
+    # Construct the full path to the file
+    file_path = os.path.join(script_dir, model_name)
+    print(file_path)
+    print(f"RT-1 Model is loaded SUCCESSFULLY from {file_path}")
     vit = MaxViT(
         num_classes = 1000,
         dim_conv_stem = 64,
@@ -44,22 +44,21 @@ def launchPytorch(sampleVideo, instructionText):
         cond_drop_prob = 0.2)
     batch = 2
     frames = 1
-    pytorchModel.load_state_dict(torch.load(f=MODEL_SAVE_PATH))
+    pytorchModel.load_state_dict(torch.load(f=file_path))
     # after much training
     pytorchModel.eval()
     eval_logits = pytorchModel(sampleVideo, instructionText, cond_scale = 3.) # classifier free guidance with conditional scale of 3
     actions = np.zeros(shape=(batch, frames, 6)) # (batch, frames, num_actions)
+    robotic_requences = []
     for batch_id in range(batch):
         for frame_no in range(frames):
             current_logits = eval_logits[batch_id][frame_no] # evaluate for the current batch and frame
             action_bins = torch.argmax(current_logits, axis=-1)
             current_actions = np.interp(action_bins, [0,255], [ACTION_LOWER_BOUND,ACTION_UPPER_BOUND]) # convert discretized actions to actual actions
             actions[batch_id][frame_no] = current_actions
-    print("HERE IS THE ROBOTIC SEQUENCES, ACCORDINGLY: ")
-    print(actions) # (batch, frames)
-    action_status = True
-    return {'succeed': action_status, 
-            "robotic_sequences": actions}
+    print(actions)
+
+    return actions
 
 
 if __name__ == '__main__':
