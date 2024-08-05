86c1ef1cbc5d2790bbd1c3bc356c55598c53fb15
batbayar enkhbaatar <baggi@haanivision.com>
2024-08-05 09:11:21 -0400

    task client has been done for seperated API calling

--- src/robotic_ai_model/robotic_ai_model/task_client.py
+++ src/robotic_ai_model/robotic_ai_model/task_client.py
@@ -8,17 +8,20 @@ import glob
 from rclpy.action.client import ClientGoalHandle
 from ament_index_python.packages import get_package_share_directory
 from robotic_msgs.action import RoboticTasks
-from robotic_ai_model.rt1_jax import load_policy
-
+# from robotic_ai_model.rt1_jax import load_policy
+import requests
+import re
+import ast
 class TaskClient(Node):
-    # Loading the model checkpoints for the model in the ROS2 node spinning
-    policy_loaded, embed = load_policy.load_check_points()
+
+    url = 'http://127.0.0.1:5000/command'
 
     def __init__(self):
         super().__init__('RoboticTaskClient_Node')
         self._action_client = ActionClient(self, RoboticTasks, 'task_server')
         self._action_client.wait_for_server()
-        self.get_logger().info('Action server ready.')
+        self.get_logger().info('Action Client ready.')
+        self.new_goal = True
 
     def send_goal(self):
         # Creating goal
@@ -31,42 +34,54 @@ class TaskClient(Node):
         
         if last_frame_path:
             try:
-                order = load_policy.main(policy=self.policy_loaded, embed=self.embed, obvers=last_frame_path, inputText=command_input)
-                ac_seq = [float(i) for i in order]
-                goal_msg.task_sequences = ac_seq
+                with open(last_frame_path, 'rb') as file:
+                    files = {'image': file}
+                    data = {'text': command_input}
+                    response = requests.post(self.url, files=files, data=data)
+                    response.raise_for_status()  # Raise an HTTPError for bad responses
+                    response_data = response.json()
+                    self.get_logger().info(f"Response from server: {response_data}")
+           
+                if 'result' in response_data:
+                    order_str = response_data['result']
+                    order = re.findall(r"[-+]?\d*\.\d+|\d+", order_str)
+                    ac_seq = [float(i) for i in order]
+                    goal_msg.task_sequences = ac_seq
+
             except Exception as e:
                 self.get_logger().error(f"Error in generating action sequences: {e}")
                 return
         else:
             self.get_logger().error("No frame found in the output directory.")
-            return
+
 
         # Sending a goal to Task Server through the ROS2 interface
+
         self.get_logger().info("Sending the goal...")
         send_goal_future = self._action_client.send_goal_async(goal_msg)
         send_goal_future.add_done_callback(self.goal_response_callback)
 
+    
     def goal_response_callback(self, future):
         try:
             goal_handle = future.result()
             if not goal_handle.accepted:
                 self.get_logger().info("Goal rejected :(")
-                return
-
             self.get_logger().info("Goal accepted :)")
             get_result_future = goal_handle.get_result_async()
             get_result_future.add_done_callback(self.goal_result_callback)
         except Exception as e:
             self.get_logger().error(f"Error in goal response callback: {e}")
-
+        
+    
     def goal_result_callback(self, future):
         try:
             result = future.result().result
             self.get_logger().info("Result: " + str(result.success))
-            self.send_goal()  # Prompt for the next goal
+            self.new_goal =True
         except Exception as e:
             self.get_logger().error(f"Exception in result callback: {e}")
-
+    
 def get_last_file(directory_path):
     try:
         # Get all files in the directory
@@ -91,8 +106,9 @@ def get_last_file(directory_path):
 def main(args=None):
     rclpy.init(args=args)
     node_client = TaskClient()
-    node_client.send_goal()
-    rclpy.spin(node_client)
+    while node_client.new_goal:
+        node_client.send_goal()
+        rclpy.spin_once(node_client, timeout_sec=0.5)
     node_client.destroy_node()
     rclpy.shutdown()
 
