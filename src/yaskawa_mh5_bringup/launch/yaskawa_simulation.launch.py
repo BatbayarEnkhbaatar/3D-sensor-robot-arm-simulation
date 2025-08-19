e320c7078950fec9d99aa05eed8461e8d741ed34
ubuntu <batbayarenkhbaatar>
2025-08-19 18:36:17 +0900

    binpicking demo with yaskawa

--- src/yaskawa_mh5_bringup/launch/yaskawa_simulation.launch.py
+++ src/yaskawa_mh5_bringup/launch/yaskawa_simulation.launch.py
@@ -1,9 +1,20 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from ament_index_python import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from moveit_configs_utils import MoveItConfigsBuilder
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
+    move_group_capabilities = {
+        "capabilities": "move_group/ExecuteTaskSolutionCapability"
+    }
+
+    is_sim = LaunchConfiguration('is_sim')
 
     gazebo = IncludeLaunchDescription(
         os.path.join(
@@ -27,17 +38,56 @@ def generate_launch_description():
             "moveit.launch.py"),
         launch_arguments={"is_sim": "True" }.items()
     )
-    remote_interface = IncludeLaunchDescription(
-        os.path.join(
-            get_package_share_directory("yaskawa_remote"), 
-            "launch", 
-            "remote_interface.launch.py"
-            )
+    # remote_interface = IncludeLaunchDescription(
+    #     os.path.join(
+    #         get_package_share_directory("yaskawa_remote"), 
+    #         "launch", 
+    #         "remote_interface.launch.py"
+    #         )
+    # )
+
+    moveit_config = (
+        MoveItConfigsBuilder("yaskawa_mh5lf", package_name="yaskawa_mh5_moveit2")
+        .robot_description(file_path=os.path.join(get_package_share_directory("yaskawa_mh5_description"), "urdf", "mh5.urdf.xacro"))
+        .robot_description_semantic(file_path="config/yaskawa_mh5lf.srdf")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .to_moveit_configs()
     )
+
+
+    # your task server (the node that calls MoveGroupInterface)
+    arm_task_server = Node(
+        package="yaskawa_mh5_task_node",
+        executable="task_server_arm_node",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {"use_sim_time": is_sim},
+            move_group_capabilities],
+        arguments=["--ros-args", "--log-level", "info"],
+    )
+    gripper_task_server = Node(
+        package="yaskawa_mh5_task_node",
+        executable="task_server_gripper_node",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {"use_sim_time": is_sim},
+            move_group_capabilities],
+        arguments=["--ros-args", "--log-level", "info"],
+    )
+
+
 
     return LaunchDescription([
             gazebo,
             controller,
             moveit,
-            remote_interface
+            arm_task_server,
+            gripper_task_server
+            # remote_interface
     ])
